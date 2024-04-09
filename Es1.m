26db6589ff10ca29023367ec15c0fc2fdd3a1e74
eldemaster <aledema01@gmail.com>
2024-04-09 14:13:35 +0200

    Fix Es3_sim + es1 update

--- Es1.m
+++ Es1.m
@@ -4,7 +4,7 @@
 % prendere esempio (ATTENZIONE: Questi articoli spesso contegono analisi piu' specifiche e complesse di quelle
 % richieste: limitarsi a quanto richiesto dall’elaborato).
 
-clear;
+clear all;
 clc
 
 % Variabili di stato:
@@ -41,20 +41,20 @@ val_iniziali=[5,5];
 
 scelta = menu("Scegli che caso analizzare:",'A: C=c e D=0', 'B: C=0 e D=d', 'C: C=c/x e D=0', 'D: C=0 e D=d/y', 'E: C=0 e D=0');
 
-syms x y  % definisco variabili di tipo simbolico
+syms x y  % Definisco variabili di tipo simbolico
 
 switch scelta
 
-    case 1 %CASO A Migrazione di Prede
+      case 1 %CASO A
 
           C = c;
           D = 0;
 
-          %     xeq1 =
-          %    -0.1000         0
+          %  xeq1 =
+          %  -0.1000         0
           %
-          %     xeq2 =
-          %     0.6667    1.1500
+          %  xeq2 =
+          %  0.6667    1.1500
 
           %  aval1 =
           %  0.1000
@@ -69,16 +69,16 @@ switch scelta
 
           f1 = @(t, s) [r*s(1) - a*s(1)*s(2) + C; b*s(1)*s(2) - m*s(2) + D];
 
-    case 2 %CASO B migrazione di Predatori
+      case 2 %CASO B
 
           C = 0;
           D = d;
 
-          %   xeq1 =
-          %          0    0.0500
+          %  xeq1 =
+          %  0    0.0500
           %
-          %   xeq2 =
-          %     0.6333    1.0000
+          %  xeq2 =
+          %  0.6333    1.0000
 
 
           %  aval1 =
@@ -93,79 +93,70 @@ switch scelta
 
           f1 = @(t, s) [r*s(1) - a*s(1)*s(2) + C; b*s(1)*s(2) - m*s(2) + D];
 
-    case 3 %CASO C Migrazione di poche Prede
+      case 3 %CASO C
 
           C = c/x;
           D = 0;
 
-          %     xeq1 =
+          %  xeq1 =
+          %  0.6667    1.2250
           %
-          %     0.6667    1.2250
-          %
-          %
-          %     xeq2 =
-          %
-          %     0.0000 - 0.3162i   0.0000 + 0.0000i
+          %  xeq2 =
+          %  0.0000 - 0.3162i   0.0000 + 0.0000i
 
-          %     aval1 =
-          %     -0.0225 + 0.1549i
-          %     -0.0225 - 0.1549i
-          %     Due autovalori complessi con parte reale negativa (-0.0225)
-          %     Stabilità: Stabile a spirale
-          %     aval2 =
-          %     0.2000 + 0.0000i
-          %     -0.2000 - 0.0949i
-          %     Stabilità: Instabile a sella
+          %  aval1 =
+          %  -0.0225 + 0.1549i
+          %  -0.0225 - 0.1549i
+          %  Due autovalori complessi con parte reale negativa (-0.0225)
+          %  Stabilità: Stabile a spirale
+          %  aval2 =
+          %  0.2000 + 0.0000i
+          %  -0.2000 - 0.0949i
+          %  Stabilità: Instabile a sella
 
           f1 = @(t, s) [r*s(1) - a*s(1)*s(2) + c/s(1); b*s(1)*s(2) - m*s(2) + D];
 
-    case 4 %CASO D Migrazione di pochi predatori
+      case 4 %CASO D
 
           C = 0;
           D = d/y;
 
-          %     xeq1 =
+          %  xeq1 =
+          %  0.6333    1.0000
           %
-          %     0.6333    1.0000
-          %
-          %
-          %      xeq2 =
-          %
-          %          0   -0.2236
+          %  xeq2 =
+          %  0   -0.2236
 
-          %   aval1 =
-          %   -0.0100 + 0.1375i
-          %   -0.0100 - 0.1375i
-          %   Stabilità: Stabile a spirale
-          %   aval2 =
-          %   -0.4000
-          %   0.1224
-          %   Stabilità: Instabile a sella
+          %  aval1 =
+          %  -0.0100 + 0.1375i
+          %  -0.0100 - 0.1375i
+          %  Stabilità: Stabile a spirale
+          %  aval2 =
+          %  -0.4000
+          %  0.1224
+          %  Stabilità: Instabile a sella
 
           f1 = @(t, s) [r*s(1) - a*s(1)*s(2) + C; b*s(1)*s(2) - m*s(2) + d/s(2)];
-    case 5  %CASO E Nessuna migrazione
+
+      case 5  %CASO E
 
           C = 0;
           D = 0;
 
-          %   xeq1 =
+          %  xeq1 =
+          %  0     0
           %
-          %      0     0
+          %  xeq2 =
+          %  0.6667    1.0000
           %
-          %    xeq2 =
+          %  aval1 =
+          %  -0.2000
+          %  0.1000
+          %  Stabilità: Instabile a sella
           %
-          %     0.6667    1.0000
-          %
-          %    aval1 =
-          %
-          %    -0.2000
-          %     0.1000
-          %     Stabilità: Instabile a sella
-          %
-          % aval2 =
-          %
-          %    0.0000 + 0.1414i
-          %    0.0000 - 0.1414i
+          %  aval2 =
+          %  0.0000 + 0.1414i
+          %  0.0000 - 0.1414i
 
           f1 = @(t, s) [r*s(1) - a*s(1)*s(2) + C; b*s(1)*s(2) - m*s(2) + D];
 
@@ -173,15 +164,15 @@ end
 
 f = [r*x - a*x*y + C; b*x*y - m*y + D];
 
-% PTI DI EQUILIBRIO
-% dx/dt=0=>f=0
-xeq_s=solve(f==0);
+% PUNTI DI EQUILIBRIO
+% dx/dt=0 => f=0
+xeq_s = solve(f==0);
 
-xeq1_s= [xeq_s.x(1), xeq_s.y(1)];
-xeq2_s= [xeq_s.x(2), xeq_s.y(2)];
+xeq1_s = [xeq_s.x(1), xeq_s.y(1)];
+xeq2_s = [xeq_s.x(2), xeq_s.y(2)];
 
-xeq1=double(xeq1_s)
-xeq2=double(xeq2_s)
+xeq1 = double(xeq1_s)
+xeq2 = double(xeq2_s)
 
 % STABILITA' PUNTI DI EQUILIBRIO
 % Per determinare la stabilità di un punto di equilibrio, calcoliamo gli 
@@ -192,15 +183,17 @@ xeq2=double(xeq2_s)
 % J = [r - a*y, -a*x]
 %     [b*y, b*x - m]
 
-% devo calcolare la jacobiana di f nelle direzioni di x,y
+% Devo calcolare la jacobiana di f nelle direzioni di x,y
 A_s = jacobian(f,[x,y]); % [x,y] indica l'ordine (e quindi la colonna) delle derivate parziali
 % Calcolo la matrice A nel pto di equilibrio 1
-A1_s=subs(A_s,[x,y],xeq1)
-A1=double(A1_s); % a questo punto A1 è una matrice di numeri reali e a me servono i suoi autovalori
-aval1=eig(A1);
-A2_s=subs(A_s,[x,y],xeq2);
-A2=double(A2_s); % a questo punto A2 è una matrice di numeri reali e a me servono i suoi autovalori
-aval2=eig(A2);
+A1_s = subs(A_s,[x,y],xeq1)
+A1 = double(A1_s); % A questo punto A1 è una matrice di numeri reali e a me servono i suoi autovalori
+aval1 = eig(A1);
+A2_s = subs(A_s,[x,y],xeq2);
+A2 = double(A2_s); % A questo punto A2 è una matrice di numeri reali e a me servono i suoi autovalori
+aval2 = eig(A2);
+
+
 
 tspan = [0 1000];
 % Risolve il sistema
